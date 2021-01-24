35ae22372d1a4880ba4f8d27cb3e7083654fc8a7
handcopper97 <61685321+handcopper97@users.noreply.github.com>
2021-01-24 15:48:31 +0900

    import logo from './logo.svg'; import './App.css';

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
+import React, {Component} from 'react';
+import MyName from "./MyName";
+
+class App extends Component {
+    render(){
+        return(
+          <MyName name = "리액트" />  
+        );
+    }
+}
+
+export default App;
+
+/*
 import logo from './logo.svg';
 import './App.css';
 
@@ -23,3 +37,4 @@ function App() {
 }
 
 export default App;
+*/
